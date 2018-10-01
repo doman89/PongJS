da506f7448a0436538e9ac754e01d0a11aa8e2bc
doman89 <mateusz.domanski.1989@gmail.com>
2018-10-01 21:46:41 +0200

    create Arrays for collision information, upgrade move() methods in objects, and run loops

--- js/script.js
+++ js/script.js
@@ -28,11 +28,16 @@ function mouseSupportForPlayer(e) {
 };
 
 function keyboardSupportForPlayer(e) {
+    console.log(e.keyCode);
     if (activeKeyboard) {
         if (e.keyCode == 87)
-            paddlePlayer.moveUp();
+            paddlePlayer.moveUp(collisionPointsY, collisionPointsX);
         else if (e.keyCode == 83)
-            paddlePlayer.moveDown();
+            paddlePlayer.moveDown(collisionPointsY, collisionPointsX);
+        if (e.keyCode == 80)
+            paddleComputer.moveUp(collisionPointsY, collisionPointsX);
+        else if (e.keyCode == 186)
+            paddleComputer.moveDown(collisionPointsY, collisionPointsX);
     }
 }
 
@@ -43,6 +48,11 @@ const clearScreen = () => {
     ctx.fillStyle = "lightgrey";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+//array with all activ game elements (paddels, balls)
+const collisionObjects = [];
+const collisionPointsX = [];
+const collisionPointsY = [];
+
 //object Paddle
 function Paddle(width, height, color, positionX, positionY) {
     this.width = width;
@@ -54,14 +64,23 @@ function Paddle(width, height, color, positionX, positionY) {
     this.middleWidth = this.width / 2;
     this.speed = 10;
 
-    this.moveUp = () => {
+    this.moveUp = (arrayY, arrayX) => {
+        for (let i = 0; i < arrayY.length; i++) {
+            if (((this.positionY - this.speed) >= arrayY[i] && (this.positionY - this.speed) <= arrayY[i]) && (this.positionX == arrayX[i]))
+                return;
+        }
         if (this.positionY - this.speed > 0)
             this.positionY -= this.speed;
         else if (this.positionY > 0)
             this.positionY = 0;
     };
 
-    this.moveDown = () => {
+
+    this.moveDown = (arrayY, arrayX) => {
+        for (let i = 0; i < arrayY.length; i++) {
+            if (((this.positionY + this.speed) >= arrayY[i] && (this.positionY + this.speed) <= arrayY[i]) && (this.positionX == arrayX[i]))
+                return;
+        }
         if (this.positionY + this.speed < canvas.height - this.height)
             this.positionY += this.speed;
         else if (this.positionY < canvas.height - this.height)
@@ -74,15 +93,23 @@ function Ball(size, color, positionX, positionY) {
     this.height = size;
     this.positionX = positionX;
     this.positionY = positionY;
+    this.newPosition = false; //disable update position, if object not move in intervals
     this.color = color;
     this.middleBall = size / 2;
-    this.speedX = 1;
-    this.speedY = 0;
+    this.speedX = 2;
+    this.speedY = 2;
     //direction true -> right, down; false -> left, up
-    this.directionX = true;
+    this.directionX = false;
     this.directionY = true;
     // 1 - player win, 2 - enemy win, 0 - not end;
-    this.move = () => {
+    this.move = (arrayX, arrayY) => {
+        for (let i = 0; i < arrayX.length; i++) {
+            if (((this.positionX - this.speedX) == arrayX[i]) && ((this.positionY - this.speedY) == arrayY[i]) || ((this.positionX + this.width + this.speedX) == arrayX[i]) && ((this.positionY + this.height + this.speedY) == arrayY[i])) {
+                // console.log("kolizja");
+                this.directionX = !this.directionX;
+                this.directionY = !this.directionY;
+            }
+        }
         if (this.directionX) {
             if (this.positionX + this.width < canvas.width) {
                 this.positionX += this.speedX;
@@ -100,6 +127,7 @@ function Ball(size, color, positionX, positionY) {
                 return 2;
             }
 
+
         }
         if (this.directionY) {
             if (this.positionY + this.height < canvas.height) {
@@ -116,30 +144,62 @@ function Ball(size, color, positionX, positionY) {
                 this.directionY = true;
             }
         }
+
     }
-}
+};
 
 const ball = new Ball(10, "black", canvas.width / 2 - 5, canvas.height / 2 - 5);
+const ball2 = new Ball(20, "blue", canvas.width - 30, canvas.height - 30);
 
-let paddlePlayer = new Paddle(10, 50, "green", 10, 10);
+const paddlePlayer = new Paddle(10, 50, "green", 10, 10);
 const paddleComputer = new Paddle(10, 50, "red", canvas.width - 20, 10);
 
+collisionObjects.push(paddlePlayer, paddleComputer, ball, ball2);
+
+const pushcollisionPoints = (collisionObjects, collisionPointsX, collisionPointsY) => {
+    collisionPointsY.length = collisionPointsX.length = 0;
+    collisionObjects.forEach(collisionObject => {
+        for (let y = 0; y < collisionObject.height; y++) {
+            for (let x = 0; x < collisionObject.width; x++) {
+                collisionPointsY.push(collisionObject.positionY + y);
+                collisionPointsX.push(collisionObject.positionX + x);
+
+            }
+        }
+
+    })
+};
+
 //function drawing object on canvas
-const drawObject = (drawingObject, context) => {
-    context.fillStyle = drawingObject.color;
-    context.fillRect(drawingObject.positionX, drawingObject.positionY, drawingObject.width, drawingObject.height);
+const drawObject = (drawingObjects, context) => {
+    drawingObjects.forEach(drawingObject => {
+        context.lineJoin = "round";
+        context.fillStyle = drawingObject.color;
+        context.fillRect(drawingObject.positionX, drawingObject.positionY, drawingObject.width, drawingObject.height);
+    })
+
 }
+
+// var console = () => {
+//     function log() {
+//         return null;
+//     }
+// }
 
 const run = () => {
     if (!(canvas.width === gameWidth))
         refreshGameWindow();
     clearScreen();
     // movePlayer(paddlePlayer);
-    ball.move();
-    paddleComputer.moveDown(10);
-    drawObject(paddlePlayer, ctx);
-    drawObject(paddleComputer, ctx);
-    drawObject(ball, ctx);
+    ball.move(collisionPointsX, collisionPointsY);
+    pushcollisionPoints(collisionObjects, collisionPointsX, collisionPointsY);
+    ball2.move(collisionPointsX, collisionPointsY);
+    pushcollisionPoints(collisionObjects, collisionPointsX, collisionPointsY);
+    // paddleComputer.moveDown(10);
+    drawObject([paddlePlayer, paddleComputer, ball, ball2], ctx);
+    // drawObject(paddleComputer, ctx);
+    // drawObject(ball, ctx);
+    // drawObject(ball2, ctx);
     // console.log(readMousePositionF);
 }
 
